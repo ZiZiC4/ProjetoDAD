12c239feac9f654e05a5cf5af78fd224c2c82516
SchoolThings98 <2181854@my.ipleiria.pt>
2020-11-18 20:04:38 +0000

    Models Relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    //perguntar amanha
+    public function products(){
+    	return $this->belongsToMany('App\Models\Product','order_items','order_id','product_id');
+    }
+
+    public function userPrepared(){
+        return $this->belongsTo('App\Models\User','prepared_by','id');
+    }
+
+    public function userDelivered(){
+        return $this->belongsTo('App\Models\User','delivered_by','id');
+    }
+
+    public function customer(){
+        return $this->belongsTo('App\Models\Customer','customer_id','id');
+    }
 }
