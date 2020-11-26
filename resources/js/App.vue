53ab699887b2a55c7afa648e705c0b4e8e8b67d0
ZiZiC4 <2181836@my.ipleiria.pt>
2020-11-26 13:15:23 +0000

    update login logout

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/users">Users</router-link>
-    <router-link to="/login">Login</router-link> 
-    <router-link to="/logout">Logout</router-link> 
+    <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/users">Users</router-link>
+        <router-link to="/login">Login</router-link>
+        <router-link to="/logout">Logout</router-link>
 
-    <router-link to="/login">Login</router-link>
+        <router-link to="/login">Login</router-link>
 
-    <router-view></router-view>
-  </div>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
 export default {
- methods: {
- logout () {
- axios.post('/api/logout').then(response => {
- console.log('User has logged out')
- })
- .catch(error => {
- console.log('Invalid Logout')
- })
- }
- }
-}
+    methods: {
+        logout() {
+            axios
+                .post("/api/logout")
+                .then(response => {
+                    console.log("User has logged out");
+                })
+                .catch(error => {
+                    console.log("Invalid Logout");
+                });
+        }
+    }
+};
 </script>
