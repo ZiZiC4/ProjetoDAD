3d863c26523c0f727ef03057e50fc8f29e777531
SchoolThings98 <2181854@my.ipleiria.pt>
2021-01-02 18:10:13 +0000

    minor stuffs

--- resources/js/components/historyList.vue
+++ resources/js/components/historyList.vue
@@ -1,10 +1,33 @@
 <template>
-  
+  <div v-if="orders">
+        <table class="table table-striped">
+            <thead>
+                <th>Order ID:</th>
+                <th>Status</th>
+                <th>Opened At:</th>
+                <th>Notes:S</th>
+                <th v-if="currentState">Choose Order</th>
+            </thead>
+            <tbody >
+                <tr v-for="(order,index) in orders" :key="index">
+                <td>{{order.id}}</td>
+                <td>{{names[index]}}</td>
+                <td>{{order.current_status_at}}</td>
+                <td  v-if="currentState"><a class="btn btn-xs btn-primary" 
+            v-on:click.prevent="selectOrder(order)">Add to shopping cart</a></td>
+                </tr>
+            </tbody>
+            </table>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return{
+                order:null,
+            }
+    },
 }
 </script>
 
